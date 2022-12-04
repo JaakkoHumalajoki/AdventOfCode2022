35a75b4bed72409b84e8a107558d4f2aa5c0d7b0
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-12-04 10:46:01 +0200

    finish puzzle 7

---
+++ puzzle7/index.ts
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+
+const overlappingPairs: string[] = []
+
+const rows = input.split('\n')
+rows.forEach((row) => {
+  const [left, right] = row.split(',')
+  const [leftMin, leftMax] = left.split('-').map(text => Number(text))
+  const [rightMin, rightMax] = right.split('-').map(text => Number(text))
+
+  if (leftMin <= rightMin && leftMax >= rightMax) {
+    overlappingPairs.push(row)
+  } else if (rightMin <= leftMin && rightMax >= leftMax) {
+    overlappingPairs.push(row)
+  }
+})
+
+console.log('Overlapping pair count:', overlappingPairs.length)
