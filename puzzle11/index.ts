f6c21d9a50bbabc5307fc9adeed61fb157a5ecb1
Jaakko Humalajoki <jaakko.humalajoki@gmail.com>
2022-12-06 10:32:46 +0200

    puzzle 11 finished

---
+++ puzzle11/index.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+
+let buffer = ""
+let answer = 0
+
+for (let i = 0; i < input.length; ++i) {
+  const char = input[i]
+  if (buffer.includes(char)) {
+    buffer += char
+    const cutPoint = buffer.indexOf(char)
+    buffer = buffer.slice(cutPoint + 1)
+  } else {
+    buffer += char
+    if (buffer.length === 4) {
+      answer = i + 1
+      break
+    }
+  }
+}
+
+console.log(buffer, answer)
